482b58a26e4b6480c87f948a2cc795b89d4761e4
David Benes (FIT) <benesdavid@cesnet.cz>
2025-07-15 08:48:52 +0200

    api docs

--- api.py
+++ api.py
@@ -1,4 +1,3 @@
-import asyncio
 from datetime import datetime, UTC
 import json
 import os
@@ -7,7 +6,6 @@ import pathlib
 
 from fastapi import (
     APIRouter,
-    BackgroundTasks,
     Depends,
     File,
     Form,
@@ -42,12 +40,21 @@ api = APIRouter(prefix="/api")
 
 @api.get("/git_sync")
 async def git_sync():
+    """
+    Pulls the latest analysis from git. This is called automatically by the
+    scheduled task (or in CI of analysis repository) but can also be called manually.
+    """
     git_repo = git.Repo(config.ANALYSIS_DIR)
     git_repo.remotes.origin.pull()
 
 
 @api.get("/howto")
 async def howto():
+    """
+    Returns the contents of the HOWTO.md file from the analysis directory.
+
+    This file should contain instructions for how to contribute to the analysis.
+    """
     howto_path = Path(config.ANALYSIS_DIR, "HOWTO.md")
     return FileResponse(howto_path)
 
@@ -57,17 +64,46 @@ async def howto():
 
 @api.get("/users")
 async def get_users(user: User = Depends(require_user)):
+    """
+    Retrieves a list of all users.
+
+    This endpoint requires authentication and returns a list of user data
+    in dictionary format. Each user in the list is represented as a dictionary
+    containing their respective details.
+    """
+
     users = await User.find().to_list()
     return [user.to_dict() for user in users]
 
 
 @api.get("/users/me")
 async def get_users_me(user: User = Depends(require_user)):
+    """
+    Retrieves the currently authenticated user.
+
+    This endpoint requires authentication and returns the user data
+    in dictionary format.
+    """
     return user.to_dict()
 
 
 @api.get("/users/{email}")
 async def get_user(email: str, user: User = Depends(require_admin)):
+    """
+    Retrieves a user by their email address.
+
+    This endpoint requires admin authentication and returns a user
+    data in dictionary format.
+
+    Args:
+        email (str): The email address of the user to retrieve.
+
+    Raises:
+        HTTPException: If the user is not found.
+
+    Returns:
+        dict: The user data.
+    """
     found = await User.find(User.email == email).first_or_none()
     if found is None:
         User.not_found(email)
@@ -76,6 +112,22 @@ async def get_user(email: str, user: User = Depends(require_admin)):
 
 @api.delete("/users/{email}")
 async def delete_user(email: str, user: User = Depends(require_admin)):
+    """
+    Deletes a user by their email address.
+
+    This endpoint requires admin authentication and deletes the user
+    associated with the given email.
+
+    Args:
+        email (str): The email address of the user to delete.
+
+    Raises:
+        HTTPException: If the user is not found.
+
+    Returns:
+        dict: A message indicating the user has been deleted.
+    """
+
     found = await User.find(User.email == email).first_or_none()
     if found is None:
         User.not_found()
@@ -87,6 +139,19 @@ async def delete_user(email: str, user: User = Depends(require_admin)):
 async def edit_user_self(
     name: str = Form(""), surname: str = Form(""), user: User = Depends(require_user)
 ):
+    """
+    Edits the currently authenticated user.
+
+    This endpoint requires authentication and updates the currently
+    authenticated user with the provided name and/or surname.
+
+    Args:
+        name (str): The new name for the user.
+        surname (str): The new surname for the user.
+
+    Returns:
+        dict: The updated user data.
+    """
     if name != "":
         user.name = name
     if surname != "":
@@ -101,6 +166,23 @@ async def edit_user_self(
 async def edit_user(
     email: str, is_admin: bool = Form(...), user: User = Depends(require_admin)
 ):
+    """
+    Edits another user's admin status.
+
+    This endpoint requires admin authentication and updates another user's
+    admin status.
+
+    Args:
+        email (str): The email address of the user to edit.
+        is_admin (bool): The new admin status.
+
+    Raises:
+        HTTPException: If the user is not found or if the currently authenticated
+            user is trying to change their own admin status.
+
+    Returns:
+        dict: A message indicating the user was updated.
+    """
     if user.email == email:
         raise HTTPException(
             status_code=403, detail="Cannot change your own admin status."
@@ -137,6 +219,28 @@ async def upload(
 ):
     # TODO: what if there is no s3? do i store files locally?
 
+    """
+    Create a new dataset and upload a file associated with it.
+
+    Args:
+        acronym (str): Unique identifier for the dataset.
+        versions (str): Comma-separated list of dataset versions.
+        title (str): Title of the dataset.
+        paper_title (str): Title of the paper associated with the dataset.
+        authors (str): Comma-separated list of authors of the dataset.
+        description (str): Short description of the dataset.
+        format (str): Format of the dataset.
+        doi (str): DOI of the dataset.
+        origins_doi (str): DOI of the paper associated with the dataset.
+        submitter (str): JSON-serialized Submitter object.
+        tags (str): Comma-separated list of tags for the dataset.
+        url (str): URL of the dataset.
+        label_name (str): Name of the label associated with the dataset.
+        file (Optional[UploadFile | None]): File to upload.
+
+    Returns:
+        dict: A message indicating the dataset was saved.
+    """
     versions_list = versions.split(",")
     versions_list = [v.strip() for v in versions_list if v.strip() != ""]
 
@@ -220,6 +324,28 @@ async def upload(
 # === DATASETS ===
 @api.get("/datasets")
 async def datasets():
+    """
+    Returns a list of datasets with their children and analysis status.
+
+    Each dataset is represented as a dictionary with the following keys:
+
+    - acronym
+    - versions
+    - label_name
+    - submitter
+    - submitter_email
+    - status
+    - analysis_status
+    - version_children
+    - is_child
+
+    The ``version_children`` key contains a list of dictionaries with the same keys
+    as above, but only for the children of the dataset (i.e., the datasets with the
+    same acronym and a larger version count).
+
+    The ``is_child`` key is a boolean indicating whether the dataset is a child or
+    parent.
+    """
     datasets = await Dataset.find(Dataset.status != DatasetStatus.REQUESTED).to_list()
 
     ret = []
@@ -263,6 +389,11 @@ async def datasets():
 
 @api.get("/datasets/{acronym:path}/{versions:path}")
 async def dataset(acronym: str, versions: str):
+    """
+    Get dataset by acronym and versions.
+
+    If versions == "*", return the root dataset (the one with the smallest version count).
+    """
     versions_list = [""] if versions == "*" else versions.split(",")
 
     dataset = await Dataset.find(
@@ -286,6 +417,25 @@ async def dataset(acronym: str, versions: str):
 # FIXME: should probably be something like /api/datasets/{acronym:path}/download
 @api.get("/files/{acronym:path}/{versions:path}")
 async def dataset_download(acronym: str, versions: str):
+    """
+    Generate a presigned URL to download a dataset file.
+
+    This endpoint retrieves the specified dataset by acronym and versions,
+    and generates a presigned URL that allows downloading the file associated
+    with the dataset from S3. The URL is valid for a limited time.
+
+    Args:
+        acronym (str): The unique identifier of the dataset.
+        versions (str): A comma-separated list of dataset versions or "*" for
+                        the root dataset.
+
+    Raises:
+        HTTPException: If the dataset or its file is not found.
+
+    Returns:
+        dict: A dictionary containing the presigned URL for the dataset file.
+    """
+
     versions_list = [""] if versions == "*" else versions.split(",")
 
     dataset = await Dataset.find(
@@ -314,6 +464,25 @@ async def dataset_download(acronym: str, versions: str):
 
 @api.get("/analysis_files/{acronym:path}/{versions:path}/{filename:path}")
 async def analysis_files_download(acronym: str, versions: str, filename: str):
+    """
+    Download a file from a dataset's analysis directory.
+
+    This endpoint retrieves the specified dataset by acronym and versions,
+    and serves the specified file from the analysis directory of the dataset.
+
+    Args:
+        acronym (str): The unique identifier of the dataset.
+        versions (str): A comma-separated list of dataset versions or "*" for
+                        the root dataset.
+        filename (str): The name of the file to be downloaded.
+
+    Raises:
+        HTTPException: If the dataset or the file is not found.
+
+    Returns:
+        FileResponse: The requested file, served with the correct Content-Type
+                     and filename.
+    """
     versions_list = [""] if versions == "*" else versions.split(",")
 
     dataset = await Dataset.find(
@@ -362,6 +531,47 @@ async def dataset_edit(
     label_name: Optional[str] = Form(""),
     user: User = Depends(require_user),
 ):
+    """
+    Edit an existing dataset.
+
+    This endpoint retrieves the specified dataset by acronym and versions,
+    and updates the dataset with the provided information. If the dataset is
+    not found, a 404 error is raised.
+
+    If the user is not an admin and the user email does not match the email of
+    the dataset submitter, a 401 error is raised.
+
+    Args:
+        curr_acronym (str): The current acronym of the dataset.
+        curr_versions (str): The current versions of the dataset, or "*" for
+                             the root dataset.
+        acronym (str): The new acronym of the dataset.
+        versions (str): The new versions of the dataset, or "*" for the root
+                        dataset.
+        title (str): The new title of the dataset.
+        paper_title (str): The new title of the paper associated with the
+                           dataset.
+        authors (str): The new authors of the dataset, comma-separated.
+        description (str): The new description of the dataset.
+        format (str): The new format of the dataset.
+        doi (str): The new DOI of the dataset.
+        origins_doi (str): The new DOI of the paper associated with the
+                           dataset.
+        tags (str): The new tags of the dataset, comma-separated.
+        url (str): The new URL of the dataset.
+        analysis_status (str): The new analysis status of the dataset.
+        label_name (str): The new label name associated with the dataset.
+        user (User): The user performing the request.
+
+    Raises:
+        HTTPException: If the dataset or its file is not found, or if the user
+                      is not authorized to edit the dataset.
+        HTTPException: If the dataset with the new acronym and versions already
+                      exists.
+
+    Returns:
+        dict: A message indicating the dataset was updated.
+    """
     curr_versions_list = [""] if curr_versions == "*" else curr_versions.split(",")
     dataset = await Dataset.find(
         Dataset.acronym == curr_acronym, Dataset.versions == curr_versions_list
@@ -457,6 +667,25 @@ async def dataset_upload_file(
     file: Optional[UploadFile | None] = File(None),
     user: User = Depends(require_user),
 ):
+    """
+    Upload a file to a dataset.
+
+    Args:
+        acronym (str): The acronym of the dataset to which to upload the file.
+        versions (str): A comma-separated list of dataset versions, or "*" for the
+            root dataset.
+        file (UploadFile, optional): The file to be uploaded. Defaults to None.
+        user (User, optional): The user performing the action. Defaults to
+            `Depends(require_user)`.
+
+    Raises:
+        HTTPException: If the dataset or file is not found, or if the user is not
+            authorized to edit the dataset.
+
+    Returns:
+        dict: A dictionary containing a message indicating the success of the
+            upload.
+    """
     versions_list = [""] if versions == "*" else versions.split(",")
     dataset = await Dataset.find(
         Dataset.acronym == acronym, Dataset.versions == versions_list
@@ -496,6 +725,24 @@ async def dataset_upload_file(
 
 @api.delete("/datasets/{acronym:path}/{versions:path}")
 async def delete(acronym: str, versions: str, user: User = Depends(require_admin)):
+    """
+    Delete a dataset.
+
+    Args:
+        acronym (str): The acronym of the dataset to be deleted.
+        versions (str): A comma-separated list of dataset versions, or "*" for the
+            root dataset.
+        user (User, optional): The user performing the action. Defaults to
+            `Depends(require_admin)`.
+
+    Raises:
+        HTTPException: If the dataset is not found, or if the user is not
+            authorized to delete the dataset.
+
+    Returns:
+        dict: A dictionary containing a message indicating the success of the
+            deletion.
+    """
     versions_list = [""] if versions == "*" else versions.split(",")
     dataset = await Dataset.find(
         Dataset.acronym == acronym, Dataset.versions == versions_list
@@ -520,6 +767,23 @@ async def create_comment(
     belongs_to: str = Form(...),
     parent_id: Optional[str] = Form(None),
 ):
+    """
+    Create a new comment associated with a dataset.
+
+    Args:
+        text (str): The content of the comment.
+        author (User): The author of the comment, automatically derived from the
+            current authenticated user.
+        belongs_to (str): The acronym of the dataset the comment belongs to.
+        parent_id (Optional[str]): The ID of the parent comment, if this is a reply.
+
+    Raises:
+        HTTPException: If the dataset the comment belongs to is not found.
+
+    Returns:
+        Comment: The created comment object.
+    """
+
     dataset = await Dataset.find(Dataset.acronym == belongs_to).first_or_none()
 
     if dataset is None:
@@ -545,6 +809,23 @@ async def edit_comment(
     author: User = Depends(require_user),
     # user_email: str = Depends(require_admin)
 ):
+    """
+    Edit an existing comment.
+
+    Args:
+        comment_id (str): The ID of the comment to be edited.
+        text (str): The new text content for the comment.
+        author (User): The user attempting to edit the comment, automatically derived
+            from the current authenticated user.
+
+    Raises:
+        HTTPException: If the comment is not found or if the user is not authorized
+            to edit the comment.
+
+    Returns:
+        Comment: The updated comment object.
+    """
+
     comment = await Comment.get(comment_id)
     if comment is None:
         Comment.not_found(comment_id)
@@ -565,6 +846,18 @@ async def edit_comment(
 
 @api.get("/comments/{acronym:path}")
 async def comments(acronym: str):
+    """
+    Retrieve all comments for a given dataset.
+
+    Args:
+        acronym (str): The unique identifier for the dataset.
+
+    Raises:
+        HTTPException: If the dataset is not found.
+
+    Returns:
+        list: A list of comment objects, sorted in descending order of date.
+    """
     dataset = await Dataset.find(Dataset.acronym == acronym).first_or_none()
 
     if dataset is None:
@@ -590,6 +883,19 @@ async def comments(acronym: str):
 # async def comments_delete(acronym: str, comment_id: str, user_email: str = Depends(require_admin)):
 @api.delete("/comments/{comment_id}")
 async def comments_delete(comment_id: str, author: User = Depends(require_user)):
+    """
+    Delete a comment.
+
+    Args:
+        comment_id (str): The unique identifier for the comment.
+        author (User): The authenticated user making the request.
+
+    Raises:
+        HTTPException: If the comment is not found or if the user is not authorized to delete the comment.
+
+    Returns:
+        dict: A dictionary containing a message with the comment ID and the phrase "deleted".
+    """
     comment = await Comment.get(comment_id)
 
     if comment is None:
@@ -620,11 +926,27 @@ async def comments_delete(comment_id: str, author: User = Depends(require_user))
 
 @api.get("/collectionTools")
 async def get_collection_tools():
+    """
+    Get a list of all collection tools.
+
+    Returns:
+        list: A list of CollectionTool objects.
+    """
     return await CollectionTool.find().to_list()
 
 
 @api.get("/collectionTools/{name}")
 async def get_collection_tool(name: str):
+    """
+    Retrieve a collection tool by its name.
+
+    Args:
+        name (str): The name of the collection tool to retrieve.
+
+    Returns:
+        CollectionTool or None: The collection tool object if found, otherwise None.
+    """
+
     return await CollectionTool.find(CollectionTool.name == name).first_or_none()
 
 
@@ -635,6 +957,21 @@ async def create_collection_tool(
     description: str = Form(""),
     known_issues: str = Form(""),
 ):
+    """
+    Create a new collection tool.
+
+    Args:
+        name (str): The name of the collection tool. Must be unique.
+        url (str): The URL for the collection tool. Defaults to an empty string.
+        description (str): The description of the collection tool. Defaults to an empty string.
+        known_issues (str): Any known issues with the collection tool. Defaults to an empty string.
+
+    Raises:
+        HTTPException: If a collection tool with the same name already exists.
+
+    Returns:
+        CollectionTool: The newly created CollectionTool object.
+    """
     if await CollectionTool.find(CollectionTool.name == name).first_or_none():
         raise HTTPException(status_code=400, detail="Collection tool already exists")
 
@@ -655,6 +992,22 @@ async def edit_collection_tool(
     known_issues: str = Form(""),
     user: User = Depends(require_admin),
 ):
+    """
+    Edit an existing collection tool.
+
+    Args:
+        name (str): The name of the collection tool to edit.
+        url (str): The new URL for the collection tool. Defaults to an empty string.
+        description (str): The new description for the collection tool. Defaults to an empty string.
+        known_issues (str): The new known issues for the collection tool. Defaults to an empty string.
+        user (User): The authenticated user making the request. Must be an admin.
+
+    Raises:
+        HTTPException: If the collection tool is not found.
+
+    Returns:
+        CollectionTool: The edited CollectionTool object.
+    """
     collection_tool = await CollectionTool.find(
         CollectionTool.name == name
     ).first_or_none()
@@ -673,6 +1026,19 @@ async def edit_collection_tool(
 
 @api.delete("/collectionTools/{name}")
 async def delete_collection_tool(name: str, user: User = Depends(require_admin)):
+    """
+    Delete an existing collection tool.
+
+    Args:
+        name (str): The name of the collection tool to delete.
+        user (User): The authenticated user making the request. Must be an admin.
+
+    Raises:
+        HTTPException: If the collection tool is not found.
+
+    Returns:
+        dict[str, str]: A dictionary with a message indicating that the collection tool has been deleted.
+    """
     collection_tool = await CollectionTool.find(
         CollectionTool.name == name
     ).first_or_none()
